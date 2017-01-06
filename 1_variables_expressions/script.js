20a710f3b8934c9efee6793db96352252e92fae9
Daniel Douglas <Daniel.Douglas@seattlecolleges.edu>
2017-01-05 19:59:19 -0800

    Add files via upload
    Html page with some JS and CSS

---
+++ 1_variables_expressions/script.js
@@ -0,0 +1,6 @@
+var firstName ="Daniel" ;
+var lastName = "Douglas" ;
+var yearsOfStudy = "5 years" ;
+var goal = "programming certificate" ;
+
+document.write("My name is" + " " + firstName + " " + lastName + " " + "and i've been studying in Seattle for" + " " + yearsOfStudy + " " + "and im currently enrolled in the" + " " + goal+".");
